a3e5512fce16afddf95479092f4935cc21e136a2
LUIS CESAR EUGENIO AMORIM <luiscesarn1@gmail.com>
2024-07-29 16:15:53 -0300

    POO estudos

---
+++ POO/animal.js
@@ -0,0 +1,35 @@
+class Animal{
+    //campos privados
+    #especie
+    #nome
+    //metodo construtor
+    constructor(especie,tamanho,cor,nome){
+        this.#especie = especie
+        this.tamanho = tamanho
+        this.cor = cor
+        this.#nome =nome
+
+    }
+    //metodos
+    falar(){
+        console.log(`O ${this.#nome} fez algum som`)
+    }
+    comer(){
+        console.log(`O ${this.#nome} comeu`)
+    }
+    dormir(){
+        console.log(`O ${this.#nome} dormiu`)
+    }
+    getNome(){
+        return this.#nome
+    }
+    setNome(nome){
+        this.#nome = nome
+    }
+}
+
+const animal1 = new Animal('gato' ,'gordo', 'laranja','Cleitinho')
+
+console.log(animal1.getNome())
+
+const animal2 = new Animal('cachorro','pequeno','branca', 'belina')
---
+++ POO/atividade1/livros.js
@@ -0,0 +1,92 @@
+// 1. Criar um novo livro : 
+    
+//     O livro deve conter :
+    
+//     - Título
+//     - Autor
+//     - Ano de Publicação
+//     - Status (disponível ou emprestado)
+// 2. Criar uma biblioteca (para reunir os livros)
+    
+//      Os objetos do tipo biblioteca contarão com as seguintes funções:
+    
+//     - adicionar livros à biblioteca
+//     - listar os livros disponíveis.
+
+class livro{
+    #status
+    constructor(titulo,autor,anoPubli,status){
+        this.titulo = titulo
+        this.autor = autor
+        this.anoPubli = anoPubli
+        this.#status = status
+    }
+    getStatus(){
+        return this.#status
+    }
+    setStatus(status){
+        this.#status = status
+        
+    }
+}
+
+class biblioteca{
+    constructor(){
+        this.livros = []
+    }
+    addlivos(livro){
+        if(livro !== undefined){this.livros.push(livro)}
+        else{
+            console.log('Não pode ser adicionado um livro')
+        }
+        
+    }
+    emprestarLivro(nomeLivro){
+        this.livros.forEach(livro =>{
+            if (livro.titulo == nomeLivro){
+                if(livro.getStatus() == 'disponíveis'){
+                    livro.setStatus('emprestado')
+                }else{
+                    console.log('livro já foi emprestado')
+                }
+            }
+        })
+    }
+    devolverLivro(nomeLivro){
+        this.livros.forEach(livro =>{
+            if (livro.titulo == nomeLivro){
+                if(livro.getStatus() == 'emprestado'){
+                    livro.setStatus('disponíveis')
+                }else{
+                    console.log('livro já foi devlvido')
+                }
+            }
+        })
+    }
+    listLivros(){
+        this.livros.forEach((livro)=>{
+            if(livro.getStatus() == 'disponíveis'){
+                console.log(livro.titulo)
+            }
+        })
+    }
+}
+const livro1 = new livro('teste livro1','eu1',2000,'disponíveis')
+const livro2 = new livro('teste livro2','eu2',2012,'emprestado')
+const livro3 = new livro('teste livro3','eu3',2003,'disponíveis')
+const livro4 = new livro('teste livro4','eu4',2014,'emprestado')
+const livro5 = new livro('teste livro5','eu5',2002,'disponíveis')
+
+const biblioteca1 = new biblioteca()
+
+biblioteca1.addlivos(livro1)
+biblioteca1.addlivos(livro2)
+biblioteca1.addlivos(livro3)
+biblioteca1.addlivos(livro4)
+biblioteca1.addlivos(livro5)
+
+biblioteca1.emprestarLivro('teste livro1')
+biblioteca1.devolverLivro('teste livro4')
+console.log(livro4.getStatus())
+
+biblioteca1.listLivros()
